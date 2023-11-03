8a8cedb25cb065fc8c212a41806ed7cc72d0fdec
Andrew Nguyen <21520144@gm.uit.edu.vn>
2023-11-03 22:48:15 +0700

    Replace update, delete and read by sequelize

--- src/services/userService.js
+++ src/services/userService.js
@@ -24,29 +24,49 @@ const createNewUser = async (username, password, email) => {
 };
 
 const getUserList = async () => {
-  let [results, fields] = await connection.query(`SELECT * FROM user`);
-  return results;
+  // let [results, fields] = await connection.query(`SELECT * FROM user`);
+  // return results;
+  let users = [];
+  users = await db.User.findAll();
+  return users;
 };
 
-const getUserById = async (id) => {
-  let [results, fields] = await connection.query(
-    `SELECT * FROM user WHERE id = ?`,
-    [id]
-  );
-  let user = results && results.length > 0 ? results[0] : {};
+const getUserById = async (userId) => {
+  // let [results, fields] = await connection.query(
+  //   `SELECT * FROM user WHERE id = ?`,
+  //   [id]
+  // );
+  // let user = results && results.length > 0 ? results[0] : {};
+  // return user;
+  let user = {};
+  user = await db.User.findOne({
+    where: { id: userId },
+  });
   return user;
 };
 
-const deleteUser = async (id) => {
-  await connection.query(`DELETE FROM user WHERE id = ?`, [id]);
+const deleteUser = async (userId) => {
+  // await connection.query(`DELETE FROM user WHERE id = ?`, [id]);
+  await db.User.destroy({
+    where: { id: userId },
+  });
 };
 
-const updateUserById = async (id, username, email) => {
-  await connection.query(
-    `UPDATE user 
-  SET username = ?, email = ?
-  WHERE id = ?`,
-    [username, email, id]
+const updateUserById = async (userId, username, email) => {
+  // await connection.query(
+  //   `UPDATE user
+  // SET username = ?, email = ?
+  // WHERE id = ?`,
+  //   [username, email, userId]
+  // );
+  await db.User.update(
+    {
+      username: username,
+      email: email,
+    },
+    {
+      where: { id: userId },
+    }
   );
 };
 
