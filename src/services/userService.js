55e421c914430f79574c0c339ca21ede98e68b45
Andrew Nguyen <21520144@gm.uit.edu.vn>
2023-11-04 00:14:06 +0700

    Small update

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,4 +1,3 @@
-import connection from "../config/database";
 import bcrypt from "bcryptjs";
 import db from "../models/index";
 
@@ -11,11 +10,6 @@ const hashUserPassword = (userPassword) => {
 
 const createNewUser = async (username, password, email) => {
   let hashPass = hashUserPassword(password);
-  // await connection.query(
-  //   `INSERT INTO user (username, password, email)
-  //       VALUES (?,?,?)`,
-  //   [username, hashPass, email]
-  // );
   await db.User.create({
     username: username,
     password: hashPass,
@@ -24,20 +18,12 @@ const createNewUser = async (username, password, email) => {
 };
 
 const getUserList = async () => {
-  // let [results, fields] = await connection.query(`SELECT * FROM user`);
-  // return results;
   let users = [];
   users = await db.User.findAll();
   return users;
 };
 
 const getUserById = async (userId) => {
-  // let [results, fields] = await connection.query(
-  //   `SELECT * FROM user WHERE id = ?`,
-  //   [id]
-  // );
-  // let user = results && results.length > 0 ? results[0] : {};
-  // return user;
   let user = {};
   user = await db.User.findOne({
     where: { id: userId },
@@ -46,19 +32,12 @@ const getUserById = async (userId) => {
 };
 
 const deleteUser = async (userId) => {
-  // await connection.query(`DELETE FROM user WHERE id = ?`, [id]);
   await db.User.destroy({
     where: { id: userId },
   });
 };
 
 const updateUserById = async (userId, username, email) => {
-  // await connection.query(
-  //   `UPDATE user
-  // SET username = ?, email = ?
-  // WHERE id = ?`,
-  //   [username, email, userId]
-  // );
   await db.User.update(
     {
       username: username,
